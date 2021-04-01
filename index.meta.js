76765b5ad724d8a32da8ebe184a8d74fc0570516
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-04-01 17:27:25 +0000

    Deploying to gh-pages from @ Snakeroom/Embellish@090afaa461766ebe1d06bd125fe5574d634c15e9 🚀

--- index.meta.js
+++ index.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Embellish
-// @version 1.0.0
+// @version 1.0.1
 // @author haykam821
 // @description Adds Reddit event embeds to old Reddit.
 // @homepage https://github.com/Snakeroom/Embellish#readme
