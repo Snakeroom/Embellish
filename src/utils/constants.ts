090afaa461766ebe1d06bd125fe5574d634c15e9
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-04-01 13:26:26 -0400

    Bump version to 1.0.1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const API_BASE = "https://second-api.reddit.com";
 export const SUBREDDIT = "second";
 export const SUBREDDIT_PATH = "/r/" + SUBREDDIT;
 
-export const VERSION = "1.0.0";
+export const VERSION = "1.0.1";
 
 export const PLATFORM = "desktop";
 export const HEIGHT = 400;
